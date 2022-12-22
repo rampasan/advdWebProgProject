0d41c30f4588d64add3c527338c0f2f1f10fd29f
rampasan <81474683+rampasan@users.noreply.github.com>
2022-12-22 15:58:58 +0800

    Final Build

--- react/src/views/Projects.jsx
+++ react/src/views/Projects.jsx
@@ -77,43 +77,49 @@ export default function Projects(){
                                                 <td className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>{project.projectSupervisor.name}</td>
                                                 <td className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>{project.projectExaminer1.name}</td>
                                                 <td className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>{project.projectExaminer2.name}</td>
-                                                <td className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>
-                                                    <Menu as='div' className='relative inline-block text-left'>
-                                                        <div>
-                                                            <Menu.Button className='inline-flex w-full justify-center rounded-full bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100'>
-                                                                <EllipsisVerticalIcon className='h-5 w-5' />
-                                                            </Menu.Button>
-                                                        </div>
-                                                        <Transition
-                                                            as={Fragment}
-                                                            enter="transition ease-out duration-100"
-                                                            enterFrom="transform opacity-0 scale-95"
-                                                            enterTo="transform opacity-100 scale-100"
-                                                            leave="transition ease-in duration-75"
-                                                            leaveFrom="transform opacity-100 scale-100"
-                                                            leaveTo="transform opacity-0 scale-95"
-                                                        >
-                                                            <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                                                                <div className='py-1'>
-                                                                    <Menu.Item>
-                                                                        {({active}) => (
-                                                                            <Link to={'/projects/'+project.id} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>
-                                                                                Edit
-                                                                            </Link>
-                                                                        )}
-                                                                    </Menu.Item>
-                                                                    <Menu.Item>
-                                                                        {({active}) => (
-                                                                            <button onClick={() => toDelete(project)} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm w-full text-left')}>
-                                                                                Delete
-                                                                            </button>
-                                                                        )}
-                                                                    </Menu.Item>
-                                                                </div>
-                                                            </Menu.Items>
-                                                        </Transition>
-                                                    </Menu>
-                                                </td>
+                                                {user.is_admin === 1 ? (
+                                                    <td className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>
+                                                        <Menu as='div' className='relative inline-block text-left'>
+                                                            <div>
+                                                                <Menu.Button className='inline-flex w-full justify-center rounded-full bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100'>
+                                                                    <EllipsisVerticalIcon className='h-5 w-5' />
+                                                                </Menu.Button>
+                                                            </div>
+                                                            <Transition
+                                                                as={Fragment}
+                                                                enter="transition ease-out duration-100"
+                                                                enterFrom="transform opacity-0 scale-95"
+                                                                enterTo="transform opacity-100 scale-100"
+                                                                leave="transition ease-in duration-75"
+                                                                leaveFrom="transform opacity-100 scale-100"
+                                                                leaveTo="transform opacity-0 scale-95"
+                                                            >
+                                                                <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                                                    <div className='py-1'>
+                                                                        <Menu.Item>
+                                                                            {({active}) => (
+                                                                                <Link to={'/projects/'+project.id} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm')}>
+                                                                                    Edit
+                                                                                </Link>
+                                                                            )}
+                                                                        </Menu.Item>
+                                                                        <Menu.Item>
+                                                                            {({active}) => (
+                                                                                <button onClick={() => toDelete(project)} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm w-full text-left')}>
+                                                                                    Delete
+                                                                                </button>
+                                                                            )}
+                                                                        </Menu.Item>
+                                                                    </div>
+                                                                </Menu.Items>
+                                                            </Transition>
+                                                        </Menu>
+                                                    </td>
+                                                ): (
+                                                    <div className='flex items-center'>
+                                                        <p className='text-sm font-medium text-red-900 px-6 py-4 text-left'>Not Allowed</p>
+                                                    </div>
+                                                )}
                                             </tr>
                                         ))}
                                         </tbody>
